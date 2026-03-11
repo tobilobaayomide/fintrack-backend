2e0cde41061ed022ab16400e355ca3f337fcfa18
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-03-11 23:43:10 +0100

    feat: setup Express server with routes and middleware

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./src/config/db.js"
+import authRoutes from "./src/routes/authRoutes.js"
+import transactionRoutes from "./src/routes/transactionRoutes.js"
+import budgetRoutes from "./src/routes/budgetRoutes.js"
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json({ message: "FinTrack API is running 🚀" })
+})
+
+app.use("/api/auth", authRoutes)
+app.use("/api/transactions", transactionRoutes)
+app.use("/api/budgets", budgetRoutes)
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" })
+})
+
+const PORT = process.env.PORT || 5001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
